79a82b599928b24eac1be1a1c44a356fb9784839
Hognze Xu <hoxu@ucsc.edu>
2018-01-14 23:08:40 -0800

    try handle no file

--- assgn-0/wc.c
+++ assgn-0/wc.c
@@ -9,6 +9,10 @@ int main(int argc, char** argv) {
     }
     const char* fileName = argv[1];
     int fd = open(fileName, O_RDONLY);
+    if (fd == -1) {
+        printf("Error: Cannot open file %s\n", fileName);
+        return 0;
+    }
     char buffer[1];
     int count = 0;
     int charCount = 0, wordCount = 0, lineCount = 0;
