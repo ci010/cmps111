99b37e4acf0153bde6444da05cd028ac4018023c
ci010 <cijhn@hotmail.com>
2018-01-17 14:12:14 -0800

    handle multiple file and stdin

--- assgn-0/wc.c
+++ assgn-0/wc.c
@@ -1,45 +1,75 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Usage: wc <filename>\n");
-        return 0;
-    }
-    const char* fileName = argv[1];
-    int fd = open(fileName, O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Error: Cannot open file %s\n", fileName);
-        return 0;
-    }
-    char buffer[512];
+#define BUFF_SIZE 1024
+char buffer[BUFF_SIZE];
+
+void wc(int fd, int *countArray)
+{
     int count = 0;
-    int charCount = 0, wordCount = 0, lineCount = 0;
     char inSpace = 1;
-    while ((count = read(fd, buffer, 512))) {
+    while ((count = read(fd, buffer, BUFF_SIZE)))
+    {
         int i = 0;
-        for (; i < count; ++i) {
+        for (; i < count; ++i)
+        {
             char c = buffer[i];
-            ++charCount;
-            switch (c) {
-                case '\n':
-                ++lineCount;
-                case '\t':
-                case ' ':
+            ++countArray[0];
+            switch (c)
+            {
+            case '\n':
+                ++countArray[1];
+            case '\t':
+            case ' ':
                 inSpace = 1;
                 break;
 
-                default:
-                if (inSpace) {
-                    ++wordCount;
+            default:
+                if (inSpace)
+                {
+                    ++countArray[2];
                     inSpace = 0;
                 }
                 break;
             }
         }
     }
-    printf("%d\t%d\t%d\n", charCount, wordCount, lineCount);
-    close(fd);
+}
+
+int main(int argc, char **argv)
+{
+    int totalArray[3];
+    int *countArray = (int *)malloc(sizeof(int) * 3);
+    if (argc == 1)
+    {
+        wc(0, countArray);
+    }
+    else
+    {
+        int ai = 0;
+        for (; ai < argc; ++ai)
+        {
+            const char *fileName = argv[ai];
+            int fd = open(fileName, O_RDONLY);
+            if (fd == -1)
+            {
+                fprintf(stderr, "wc: %s: open: No such file or directory\n", fileName);
+            }
+            countArray[0] = countArray[1] = countArray[2] = 0;
+            wc(fd, countArray);
+            totalArray[0] += countArray[0];
+            totalArray[1] += countArray[1];
+            totalArray[2] += countArray[2];
+            printf("%d\t%d\t%d %s\n", countArray[0], countArray[1], countArray[2], fileName);
+            close(fd);
+        }
+        if (argc > 2)
+        {
+            printf("%d\t%d\t%d total\n", totalArray[0], totalArray[1], totalArray[2]);
+        }
+    }
+    free(countArray);
     return 0;
 }
