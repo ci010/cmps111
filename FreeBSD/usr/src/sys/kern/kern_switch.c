ce171276c2f18e6108ce7bd9445dc092163e2661
ci010 <cijhn@hotmail.com>
2018-02-17 17:30:43 -0800

    test if the foreach err

--- FreeBSD/usr/src/sys/kern/kern_switch.c
+++ FreeBSD/usr/src/sys/kern/kern_switch.c
@@ -513,13 +513,13 @@ runq_choose_lott(struct runq* rq) {
 	if (!TAILQ_EMPTY(rqh)) {
 		sum = 0;
 		r = random() % rq->rq_tickets;
-		// TAILQ_FOREACH(td, rqh, td_runq) {
+		TAILQ_FOREACH(td, rqh, td_runq) {
 		// 	if (sum >= r) {
 		// 		KASSERT(td != NULL, ("runq_choose: no thread on lottory queue"));
 		// 		return (td);
 		// 	}
 		// 	sum += td->td_ticket;
-		// }
+		}
 		td = TAILQ_FIRST(rqh);
 		return (td);
 	}
