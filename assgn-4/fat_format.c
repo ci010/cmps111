18c0d8046b7b532a346084b83062c9844a093e68
ci010 <cijhn@hotmail.com>
2018-03-17 21:41:42 -0700

    try finish format

--- assgn-4/fat_format.c
+++ assgn-4/fat_format.c
@@ -16,10 +16,15 @@ int main(int argc, char** argv) {
     }
     int fd;
     int N;
+    int k;
+    /**
+     * 4KB for a block 
+     */
+    uint8_t flush_area[FAT_BLOCK_SIZE];
 
     fd = open(argv[1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 
-    if(fd == 0) {
+    if (fd == 0) {
         printf("Cannot open file %s\n", argv[1]);
         return -1;
     }
@@ -29,7 +34,7 @@ int main(int argc, char** argv) {
     /**
      * init the super block
      */
-    int k = N / 1024;
+    k = N / 1024;
     if (k == 0)
         k = 1;
 
@@ -40,23 +45,37 @@ int main(int argc, char** argv) {
         .fat_block_size = FAT_BLOCK_SIZE,
         .fat_block_start = k + 1
     };
-
-    write(fd, super, 20);
-
-    int32_t file_alloc_table[1024];
+    memset(flush_area, 0, FAT_BLOCK_SIZE);
+    memcpy(flush_area, super, 20);
+    write(fd, flush_area, FAT_BLOCK_SIZE);
 
     /**
-     * In file alloc table, first is super block
+     * init FAT;
+     * in file alloc table, first is super block
      * then k block for table, marked as -1
      */
-    memset(file_alloc_table, -1, k + 1);
+    u_int32_t* fat_flush_area = (u_int32_t*)flush_area;
+    int remain = k;
+    for (int i = 0; i < k + 1; i++) {
+        memset(flush_area, 0, FAT_BLOCK_SIZE);
+        /**
+         * use remain to keep track how many FAT pointer we need to set
+         */
+        if (remain > 0) {
+            int count = remain % 1024;
+            if (count == 0) count = 1024;
+            for (int j = 0; j < count; ++i) fat_flush_area[j] = -1;
+            remain -= 1024;
+        } 
+        write(fd, flush_area, FAT_BLOCK_SIZE);
+    }
 
     /**
      * Initialize the . and .. dir
      */
     time_t t = time(0);
 
-    fat_dir dir_current = {
+    struct fat_entry dir_current = {
         .fat_dir_atime = t,
         .fat_dir_ctime = t,
         .fat_dir_mtime = t,
@@ -65,7 +84,7 @@ int main(int argc, char** argv) {
         .fat_dir_flags = 1,
         .fat_dir_unsed = 0,
     };
-    fat_dir dir_parent = {
+    struct fat_entry dir_parent = {
         .fat_dir_atime = t,
         .fat_dir_ctime = t,
         .fat_dir_mtime = t,
@@ -74,12 +93,18 @@ int main(int argc, char** argv) {
         .fat_dir_flags = 1,
         .fat_dir_unsed = 0,
     };
-
     strcpy(dir_current.fat_dir_name, '.');
     strcpy(dir_parent.fat_dir_name, '..');
 
-    write(fd, dir_current, 64);
-    write(fd, dir_parent, 64);
+    memset(flush_area, 0, FAT_BLOCK_SIZE);
+    memcpy(flush_area, dir_current, 64);
+    memcpy(flush_area + 64, dir_parent, 64);
+    
+    write(fd, flush_area, FAT_BLOCK_SIZE);
+
+    memset(flush_area, 0, FAT_BLOCK_SIZE);
+    for (int i = 0; i < N - 1; ++i)
+        write(fd, flush_area, FAT_BLOCK_SIZE);
 
     close(fd);
 }
